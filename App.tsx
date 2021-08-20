5479bb90d0a2456b413f799ab5daed3e5dc3d49d
Pedro Guimarães <moisespedro15@gmail.com>
2021-08-20 00:41:55 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import {Pokedex} from './scenes/Pokedex';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Pokedex />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ scenes/Pokedex.tsx
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import {View, Text, Image} from 'react-native';
+export const Pokedex = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([] as any[]);
+
+  getPokemons(10).then(pokemons => setData(pokemons));
+  console.log(data);
+  return (
+    <View>
+      {data.map(pokemon => (
+        <View key={pokemon.name}>
+          <View style={{flex: 1}}>
+            <Text>{pokemon.name}</Text>
+            <Image
+              source={{uri: pokemon.imageUrl}}
+              style={{width: 40, height: 40}}
+            />
+          </View>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const getPokemons = (limit: number) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+    .then(response => response.json())
+    .then(({results}) =>
+      Promise.all(results.map(({url}) => getPokemonByUrl(url))),
+    )
+    .catch(error => []);
+};
+
+const getPokemonByUrl = (url: string) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then(pokemon => {
+      const pokemon2 = {
+        name: pokemon.name,
+        imageUrl: pokemon.sprites.other['official-artwork'].front_default,
+      };
+      return pokemon2;
+    });
+};
