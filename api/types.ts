89ee805a0418a1f3cd5d40caaaf7143f075b5fe0
Pedro Guimarães <moisespedro15@gmail.com>
2021-09-02 19:08:27 -0300

    Improve GetPokemonByURLResponse
    co-authored-by: Afonso Ferrer <ferrer.afonso@gmail.com>

--- api/types.ts
+++ api/types.ts
@@ -30,34 +30,44 @@ interface PokeType {
   url: string;
 }
 
-// @TODO: improve type
-export interface GetPokemonByURLResponse {
-  name: string;
-  sprites: {
-    other: {
-      'official-artwork': {
-        front_default: string;
-      };
+interface RawSprite {
+  other: {
+    'official-artwork': {
+      front_default: string;
     };
   };
+}
+
+interface RawAbility {
+  is_hidden: boolean;
+  slot: number;
+  ability: {
+    name: string;
+    url: string;
+  };
+}
+
+interface RawStats {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+interface RawType {
+  slot: number;
+  type: {name: string; url: string};
+}
+
+export interface GetPokemonByURLResponse {
+  name: string;
+  sprites: RawSprite[];
   base_experience: number;
-  abilities: {
-    is_hidden: boolean;
-    slot: number;
-    ability: {
-      name: string;
-      url: string;
-    };
-  }[];
-  stats: {
-    base_stat: number;
-    effort: number;
-    stat: {
-      name: string;
-      url: string;
-    };
-  }[];
-  types: {slot: number; type: {name: string; url: string}}[];
+  abilities: RawAbility[];
+  stats: RawStats[];
+  types: RawType[];
 }
 
 export interface GetPokemonByNameResponse {}
