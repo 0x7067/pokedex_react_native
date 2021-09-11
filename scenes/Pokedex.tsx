35f0f7513bb1c52564050839db9ec553f0476898
Pedro Guimarães <moisespedro15@gmail.com>
2021-09-11 19:55:08 -0300

    Show network error dialog
    co-authored-by: Afonso Ferrer <ferrer.afonso@gmail.com>

--- scenes/Pokedex.tsx
+++ scenes/Pokedex.tsx
@@ -13,6 +13,7 @@ import {getPokemons} from '../api';
 import {BasicPokemonInfo} from '../types';
 import {Spinner} from '../components/Spinner';
 import {PokemonButton} from '../components/PokemonButton';
+import Dialog from 'react-native-dialog';
 
 export const Pokedex: VFC = ({navigation}: any) => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -23,17 +24,24 @@ export const Pokedex: VFC = ({navigation}: any) => {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([] as BasicPokemonInfo[]);
   const [next, setNext] = useState('');
+  const [error, setError] = useState<boolean>(false);
 
   //@TODO: Graceful exit
-  getPokemons(40).then(({data, next, error}) => {
-    console.log(error);
-    data &&
-      data.then(pokemons => {
-        setLoading(false);
-        setData(pokemons);
-        setNext(next ?? '');
-      });
-  });
+  getPokemons(40)
+    // @TODO: react query
+    .then(({data, next, error}) => {
+      //@TODO: deal with this error
+      data &&
+        data.then(pokemons => {
+          setLoading(false);
+          setData(pokemons);
+          setNext(next ?? '');
+        });
+    })
+    .catch(() => {
+      setLoading(false);
+      setError(true);
+    });
 
   const handleOnListEnd = () => {
     if (next) {
@@ -53,27 +61,35 @@ export const Pokedex: VFC = ({navigation}: any) => {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+
+      {/* @TODO: Retry when error happens */}
+      <Dialog.Container visible={error}>
+        <Dialog.Title>Network Error</Dialog.Title>
+        <Dialog.Description>
+          Something wrong happened. Please try again.
+        </Dialog.Description>
+        <Dialog.Button label="OK" onPress={() => setError(false)} />
+      </Dialog.Container>
+
       <View
         style={{
           backgroundColor: isDarkMode ? Colors.black : Colors.white,
         }}>
-        <View>
-          <FlatList
-            numColumns={3}
-            data={data}
-            keyExtractor={pokemon => pokemon.name}
-            // @TODO: Extract function
-            renderItem={({item: pokemon}) => (
-              <PokemonButton
-                // @TODO: Improve navigation functions with proper types
-                onPressCallback={() => {
-                  navigation.navigate('PokedexEntry', {pokemon});
-                }}
-                pokemon={pokemon}
-              />
-            )}
-          />
-        </View>
+        <FlatList
+          numColumns={3}
+          data={data}
+          keyExtractor={pokemon => pokemon.name}
+          // @TODO: Extract function
+          renderItem={({item: pokemon}) => (
+            <PokemonButton
+              // @TODO: Improve navigation functions with proper types
+              onPressCallback={() => {
+                navigation.navigate('PokedexEntry', {pokemon});
+              }}
+              pokemon={pokemon}
+            />
+          )}
+        />
       </View>
     </SafeAreaView>
   );
